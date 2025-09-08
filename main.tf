915d2a63ab0386d131675a1a0a1aa97e3820c7f1
Jira-saki <geepep@gmail.com>
2025-09-08 11:18:32 +0900

    config and add gitignore

--- main.tf
+++ main.tf
@@ -10,12 +10,12 @@ terraform {
   # This is critical for running Terraform in a CI/CD pipeline.
   # REPLACE "your-unique-terraform-state-bucket-name" with a globally unique S3 bucket name.
   backend "s3" {
-    bucket = "your-unique-terraform-state-bucket-name"
+    bucket = "jira-terraform-state-bucket"
     key    = "global/s3/terraform.tfstate"
     region = "us-east-1" # You can change this, but us-east-1 is common for global resources.
   }
 }
 
 provider "aws" {
-  region = "us-east-1" # Main region for your resources.
+  region = "ap-northeast-3" # Main region for your resources.
 }
